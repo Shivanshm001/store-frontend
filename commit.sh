eba476948e4128b5ed265f1f7c4294a7fa6ab48f
Shivansh Mishra <Shivanshmishra.web@gmail.com>
2023-09-20 14:14:22 +0530

    Modified commit.sh

--- commit.sh
+++ commit.sh
@@ -3,35 +3,33 @@
 echo "Current working directory : "
 pwd
 
-echo "Continue? (y/n) : "
-read input
-
-# Convert input to lowercase
-input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
-
-if [[ "$input" == "n" ]]; then
-    exit
-else
-    git status
-
-    echo -e "Adding files...\n"
-    git add .
-
-    echo "Enter commit message : "
-    read message
-
-    echo "Committing changes... "
-
-    echo "Push to git repo? (y/n) : "
-    read push
-
-    # Convert push to lowercase
-    push=$(echo "$push" | tr '[:upper:]' '[:lower:]')
-
-    if [[ "$push" == "y" ]]; then
-        git commit -m "$message"
-        git push -u origin
-    else
+function get_confirmation() {
+    echo -n "$i"
+    read input
+    # Convert input to lowercase
+    input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
+    if [[ "$input" == "n" ]]; then
         exit
     fi
-fi
+}
+
+git status
+
+get_confirmation "Continue? (y/n)? : "
+
+echo -e "\nAdding changes...\n"
+git add .
+
+get_confirmation "Commit changes? (y/n) : "
+echo
+echo -n "Enter commit message : "
+read message
+echo "Committing changes... "
+
+sleep 2
+
+git commit -m "$message"
+
+get_confirmation "Push to current active repo? (y/n)? : "
+sleep 2
+git push -u origin
