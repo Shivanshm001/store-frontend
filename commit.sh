f1098b301df434cabea0c4b1b24a0a2136fe0191
Shivansh Mishra <Shivanshmishra.web@gmail.com>
2023-09-20 14:19:45 +0530

    Modified commit.sh

--- commit.sh
+++ commit.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-echo "Current working directory : "
-pwd
-
 function get_confirmation() {
-    echo -n "$i"
+    echo -ne "$1"
     read input
     # Convert input to lowercase
     input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
@@ -13,23 +9,28 @@ function get_confirmation() {
     fi
 }
 
+echo "Current working directory : "
+pwd
+
+get_confirmation "\nContinue? (y/n)? : "
+
+
 git status
 
-get_confirmation "Continue? (y/n)? : "
-
+get_confirmation "\nAdd changes? (y/n)? : "
+sleep 1
 echo -e "\nAdding changes...\n"
 git add .
 
-get_confirmation "Commit changes? (y/n) : "
-echo
-echo -n "Enter commit message : "
+
+get_confirmation "\nCommit changes? (y/n) : "
+echo -ne "\nEnter commit message : "
 read message
-echo "Committing changes... "
-
-sleep 2
-
+echo -ne "\nCommitting changes... "
+sleep 1
 git commit -m "$message"
 
-get_confirmation "Push to current active repo? (y/n)? : "
-sleep 2
+
+get_confirmation "\nPush to current active repo? (y/n)? : \n"
+sleep 1
 git push -u origin
