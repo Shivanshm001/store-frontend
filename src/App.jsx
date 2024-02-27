57395e6a2d0d8d4d38f8895056ceafe59a378554
Shivansh mishra <Shivanshmishra.chd@gmail.com>
2024-02-27 17:16:32 +0530

    Install package vite-tsconfig-paths as --save-dev, Create jsconfig.json file, Add path alias in jsconfig.json , Update vite.config.json to include plugin 'vite-tsconfig-paths', Replace relative paths with path alias

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 
-import { AddProduct } from './components/AddProduct/AddProduct';
-import Layout from './components/Layout/Layout';
+import { AddProduct } from 'components/AddProduct/AddProduct';
+import Layout from 'components/Layout/Layout';
 
 //Auth
-import { Login } from './components/Auth/Login/Login';
-import { Register } from './components/Auth/Register/Register';
-import { RequireAuth } from './components/Auth/RequireAuth/RequireAuth';
+import { Login } from 'components/Auth/Login/Login';
+import { Register } from 'components/Auth/Register/Register';
+import { RequireAuth } from 'components/Auth/RequireAuth/RequireAuth';
 
-import { Contact } from './components/Contact/Contact';
-import { Home } from './components/Home/Home';
-import { UpdateProduct } from './components/UpdateProduct/UpdateProduct';
+import { Contact } from 'components/Contact/Contact';
+import { Home } from 'components/Home/Home';
+import { UpdateProduct } from 'components/UpdateProduct/UpdateProduct';
 
 //Hooks
-import { useDocumentTitle } from './hooks/useDocumentTitle';
+import { useDocumentTitle } from 'hooks//useDocumentTitle';
 
 //Pages
-import { Cart } from './components/Pages/Cart/Cart';
-import { Checkout } from './components/Pages/Checkout/Checkout';
-import { NavPages } from './components/Pages/NavPages';
-import { Wishlist } from './components/Pages/Wishlist/Wishlist';
-import { ShopPage } from './components/Shop/Shop';
-import { About } from './components/About/About';
-import { SingleProduct } from './components/SingleProduct/SingleProduct';
-import { PageNotFound } from './components/PageNotFound/PageNotFound';
+import { Cart } from 'components/Pages/Cart/Cart';
+import { Checkout } from 'components/Pages/Checkout/Checkout';
+import { NavPages } from 'components/Pages/NavPages';
+import { Wishlist } from 'components/Pages/Wishlist/Wishlist';
+import { ShopPage } from 'components/Shop/Shop';
+import { About } from 'components/About/About';
+import { SingleProduct } from 'components/SingleProduct/SingleProduct';
+import { PageNotFound } from 'components/PageNotFound/PageNotFound';
 
 //Route enums
 import { ABOUT, ADD_PRODUCT, CART, CHECKOUT_DETAILS, CHECKOUT_PAYMENT, CONTACT, DEFAULT, HOME, LOGIN, REGISTER, SHOP, SINGLE_PRODUCT, UPDATE_PRODUCT, WISHLIST, UNKNOWN_OR_INVALID } from './config/urlPaths';
-import { Payment } from './components/Pages/Checkout/Payment/Payment';
+import { Payment } from 'components/Pages/Checkout/Payment/Payment';
 
 export function App() {
   useDocumentTitle("Store");
